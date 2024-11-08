2ee481bfafa356bbf6aea327fdf39dd741d4a170
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-08 21:19:10 +0200

    1

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,6 +5,24 @@ import re
 import logging
 
 
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''function called format that returns the log message obfuscated'''
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
+
+
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
     '''function called filter_datum that returns the log message obfuscated'''

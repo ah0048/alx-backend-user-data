49a4b99d87d46daa52a7825df93a7a66833813a0
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-08 22:29:03 +0200

    6

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -9,3 +9,10 @@ def hash_password(password: str) -> bytes:
     '''function called hash_password that expects one string argument name
     password and returns a salted, hashed password, which is a byte string.'''
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''function called is_valid that expects 2 arguments: hashed_password,
+    which is a bytes type, and password, which is a string type. This function
+    returns a boolean.'''
+    return bcrypt.checkpw(password.encode(), hashed_password)

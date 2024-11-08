04d8a6326b6c9bd741e71fd92457176a2fb890ae
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-08 20:35:14 +0200

    0

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''function called filter_datum that returns the log message obfuscated'''
+from typing import List
+import re
+import logging
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    '''function called filter_datum that returns the log message obfuscated'''
+    for field in fields:
+        message = re.sub(rf'{field}=.*?{separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message

51f2f2ecc384beee8f7721241b427da162ee6343
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-12 20:45:10 +0200

    3

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,12 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         '''require auth paths'''
-        return False
+        if path is None or excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        for excluded_path in excluded_paths:
+            if path.rstrip('/') == excluded_path.rstrip('/'):
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         '''header for auth'''

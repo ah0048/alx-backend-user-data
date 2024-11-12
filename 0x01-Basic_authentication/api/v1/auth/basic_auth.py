96cb2032c7299dfb99566dc751a38391cf56d900
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-12 23:20:45 +0200

    9

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import binascii
 import base64
+from typing import Tuple
 
 
 class BasicAuth(Auth):
@@ -42,3 +43,19 @@ class BasicAuth(Auth):
             return decoded_string.decode('utf-8')
         except (binascii.Error, UnicodeDecodeError):
             return None
+
+    def extract_user_credentials(
+            self,
+            decoded_base64_authorization_header: str
+            ) -> Tuple[str, str]:
+        '''returns the user email and password from decoded value.'''
+        if decoded_base64_authorization_header is None:
+            return (None, None)
+        elif not isinstance(decoded_base64_authorization_header, str):
+            return (None, None)
+        elif ":" not in decoded_base64_authorization_header:
+            return (None, None)
+        return (
+            decoded_base64_authorization_header.split(":")[0],
+            decoded_base64_authorization_header.split(":")[1]
+        )

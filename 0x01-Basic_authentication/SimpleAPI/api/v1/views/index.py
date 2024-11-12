65de58483320fa33253ee3ae6954cc10c69015c5
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-12 18:15:11 +0200

    1

--- 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unAuth():
+    """ GET /api/v1/unauthorized
+    Return:
+      - unauthorized 401
+    """
+    abort(401)

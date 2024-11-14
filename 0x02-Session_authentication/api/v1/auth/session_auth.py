7df2ca1ff107c014613c94d264845162dfde68ae
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-14 22:35:53 +0200

    0

---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+'''session auth class'''
+from api.v1.auth.auth import Auth
+from typing import Tuple, TypeVar
+from models.user import User
+
+
+class SessionAuth(Auth):
+    '''session auth class'''
+    pass

efe8ad5dde92402f146ae39e357daf990f93ac65
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-17 13:44:25 +0200

    5

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -37,13 +37,13 @@ def filter_requests(request=request):
         ]
     if not auth.require_auth(request.path, excluded_paths):
         return
+    cookie = auth.session_cookie(request)
+    if auth.authorization_header(request) is None and cookie is None:
+        abort(401)
     if auth.authorization_header(request) is None:
         abort(401)
     if auth.current_user(request) is None:
         abort(403)
-    cookie = auth.session_cookie(request)
-    if auth.authorization_header(request) is None and cookie is None:
-        abort(401)
     setattr(request, "current_user", auth.current_user(request))
 
 

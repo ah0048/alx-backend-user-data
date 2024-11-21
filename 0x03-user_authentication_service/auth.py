d6d06632f92819eae60d8f6e6a4018ddffc4575f
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-21 22:06:44 +0200

    13

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -52,6 +52,11 @@ class Auth:
         except NoResultFound:
             return None
 
+    def destroy_session(self, user_id: int) -> None:
+        """Destroy a session
+        """
+        self._db.update_user(user_id, session_id=None)
+
 
 def _hash_password(password: str) -> bytes:
     """Takes in a password string arguments and returns bytes

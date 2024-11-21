e4220b9c600f0c441c192c5aa9521f71c861ee06
Ahmed Harhash <ahmedharhash0048@gmail.com>
2024-11-21 22:01:39 +0200

    12

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -44,6 +44,14 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Get a user from a session ID
+        """
+        try:
+            return self._db.find_user_by(session_id=session_id)
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """Takes in a password string arguments and returns bytes
